f8368041d4726f77d5cb21f765a97ee012b22134
sebelumya <hamzahmuhajirin@gmail.com>
2022-06-10 16:19:57 +0700

    javascript adalah jalan ninjaku

--- promise.js
+++ promise.js
@@ -1,7 +1,30 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    let total = 0;
+    promiseTheaterIXX()
+      .then((hasilSetelahMenonton) => {
+        hasilSetelahMenonton.forEach((item) => {
+          if (item.hasil === emosi) {
+            total += 1;
+          }
+        });
+      })
+      .then(
+        promiseTheaterVGC().then((hasilSetelahMenonton) => {
+          hasilSetelahMenonton.forEach((item) => {
+            if (item.hasil === emosi) {
+              total += 1;
+            }
+          });
+          resolve(total);
+        })
+      );
+  });
+};
 
 module.exports = {
   promiseOutput,
